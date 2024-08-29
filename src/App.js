82b0da88e8cac079c66d9e00746ab4629d3b5172
saad8212 <saad.shabbir1435@gmail.com>
2024-08-30 04:45:48 +0500

    table route added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/table" element={<SurveyTable />} />
       </Routes>
     </Router>
